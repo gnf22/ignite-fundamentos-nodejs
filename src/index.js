e69ac4a0d2e8477dc37722a0144349b4832233e6
gnf22 <gnferreira2000@gmail.com>
2021-03-08 19:50:47 -0300

    Finished project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/courses', (request, response) => {
+  const query = request.query;
+
+  console.log({ query });
+
   return response.json([
     'Curso 1',
     'Curso 2',
@@ -11,6 +17,10 @@ app.get('/courses', (request, response) => {
 });
 
 app.post('/courses', (request, response) => {
+  const { name, age } = request.body;
+
+  console.log({ name, age });
+
   return response.json([
     'Curso 1',
     'Curso 2',
@@ -20,6 +30,10 @@ app.post('/courses', (request, response) => {
 });
 
 app.put('/courses/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log({ id });
+
   return response.json([
     'Curso 6',
     'Curso 2',
